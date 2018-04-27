d595edf5e632ccfa97cf275557fb7193608aeaa9
Saran <saran.rk@gmail.com>
2018-04-27 16:07:15 +0530

    Added AuthContext and UserModel

---
+++ IntellectBit.Treeshrew.MyHomeService/AuthContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace IntellectBit.Treeshrew.MyHomeService
+{
+    public class AuthContext : IdentityDbContext<IdentityUser>
+    {
+        public AuthContext()
+            : base("AuthContext")
+        {
+
+        }
+    }
+}
